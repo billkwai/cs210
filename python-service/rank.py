78ed6009002e35a9b607291523faecbc9235e6a2
Neel Bedekar <neel.bedekar@gmail.com>
2018-04-19 15:08:33 -0700

    Finished prelim ranking algorithm; updates exactly as expected.

--- python-service/rank.py
+++ python-service/rank.py
@@ -23,9 +23,9 @@ cur = conn.cursor()
 cur.execute('''SELECT MAX(id) AS max FROM PLAYERS''')
 
 max = cur.fetchone()['max']
-print (max)#(len(result))
+#print (max)#(len(result))
 
-PLAYERS_AT_ONCE = 10
+PLAYERS_AT_ONCE = 100
 
 i = 0
 while (i < max):
@@ -38,7 +38,7 @@ while (i < max):
             cur_string += str(i+j) + ", "
         else:
             cur_string += str(i+j) + ");"
-    print (cur_string)
+    #print (cur_string)
     cur.execute(cur_string)
     rv = cur.fetchall()
     if rv is None:
@@ -48,6 +48,8 @@ while (i < max):
     #print (len(rv))
     for j in range(0, len(rv)):
         pick = rv[j]
+        #if (pick['player_id'] == 67):
+            #print(pick['days'])
         #print (pick)
         time_factor = float(365 - pick['days']) / 100.0
         pick_factor = 0.0
@@ -55,10 +57,11 @@ while (i < max):
             pick_factor = float(pick['correct_payout'])
         else:
             chance_other_win = 1.0 - (float(pick['bet_size']) / float(pick['correct_payout']))
-            pick_factor = (float(pick['correct_payout']) / chance_other_win) * -1.0
+            pick_factor = (float(pick['bet_size']) / chance_other_win) * -1.0
 
         pick_factor /= 100.0
-        #print(pick_factor, time_factor)
+        #if (pick['player_id'] == 67):
+            #print(pick_factor, time_factor)
         scores[pick['player_id'] % PLAYERS_AT_ONCE - 1][0] += pick_factor * time_factor
 
         #print(score)
@@ -71,8 +74,29 @@ while (i < max):
         if (scores[j][0] < 0 and freq_factor != 0):
             freq_factor = 1.0 / freq_factor
         scores[j][2] = scores[j][0] * freq_factor
-        i += 1
-    print (scores)
+
+    #print (scores)
+    if (np.sum(scores, axis=0)[1] != 0):
+        
+        update_str = '''UPDATE PLAYERS SET score = (CASE id'''
+        second_part_str = ""
+
+        for j in range(0, min(PLAYERS_AT_ONCE, max - i)):
+            if (scores[j][1] != 0):
+                update_str += " WHEN " + str(j + 1 + i) + " THEN " + str(scores[j][2])
+                second_part_str += str(j + 1 + i) + ", "
+
+        update_str += ' END) WHERE id IN ('
+        second_part_str = second_part_str[0:len(second_part_str)-2]
+
+        update_str = update_str + second_part_str
+        update_str += ");"
+
+        cur.execute(update_str)
+        conn.commit()
+        #print(update_str)
+
+    i += min(PLAYERS_AT_ONCE, max - i)
 
 cur.close()
 conn.close()
