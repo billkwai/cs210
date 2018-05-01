96d9842b0e133c7328687d2beae2c27a3ef243ed
Bill Kwai <billkwai.edu@gmail.com>
2018-05-01 01:07:33 -0700

    adds initial skeleton for implementing explore collection view for event categories

---
+++ LynxApp/LynxApp/Controller/Home/Collections/ExploreEventCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreEventCollectionViewCell.swift
+//  LynxApp
+//
+//  Created by Bill Kwai on 5/1/18.
+//  Copyright © 2018 Colin Dolese. All rights reserved.
+//
+
+import UIKit
+
+class ExploreEventCollectionViewCell: UICollectionViewCell {
+    
+}
